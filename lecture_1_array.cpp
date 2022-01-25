7e4fe9fc52ba87761b7d9159f2288709620c9c9e
Youngdae Lee <ilmare0916@hotmail.com>
2022-01-25 21:41:57 +0900

    implement binary search

---
+++ lecture_1_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int binary_search() {
+    cout<<"run binary_search"<<endl;
+    int target = 15;
+    int input[] = {1, 3, 5, 6, 7, 15, 20};
+
+    int left = 0;
+    int right = sizeof(input) / sizeof(int) - 1;
+
+    while(left <= right) {
+        int pivot = (left + right) / 2;
+        if(input[pivot] == target) {
+            return pivot;
+        } else if(input[pivot] < target) {
+            left = pivot + 1;
+        } else {
+            right = pivot - 1;
+        }
+    }
+    return -1;
+
+}
+
+
+int main() {
+    cout<<"Hello World"<<endl;
+    int rtn = binary_search();
+    cout<<"rtn = "<<rtn<<endl;
+    return 0;
+}
