48372c6723308bca6c4a67c4ce0be1db255342c4
Youngdae Lee <ilmare0916@hotmail.com>
2022-02-15 20:40:33 +0900

    test for best3

--- 3best.cpp
+++ 3best.cpp
@@ -1,10 +1,18 @@
+//
+//  main.cpp
+//  best3
+//
+//  Created by Youngdae Lee on 2022. 2. 15..
+//  Copyright © 2022년 Youngdae Lee. All rights reserved.
+//
 #include <iostream>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
 const int MAX_CITY = 20;
-
+const int MAX_RESTAURANT = 10;
 struct Restaurant {
     int cityID;
     string name;
@@ -18,29 +26,70 @@ struct Restaurant {
 
 struct cmp {
     bool operator()(Restaurant u, Restaurant t) {
-        return u.value > t.value;
+        if( u.value == t.value) {
+            return u.cityID < t.cityID;
+        } else {
+            return u.value < t.value;
+        }
     }
 };
 
+
 priority_queue<Restaurant, vector<Restaurant>, cmp> pq[MAX_CITY + 1];
+vector<int> mArea;
+unordered_map<string, int> restaurantList;
 
-int main() {
+int main(int argc, const char * argv[]) {
+    
+    int area = 0;
+    mArea.push_back(area);
+    pq[area].push(Restaurant(1, "apple", 10));
+    restaurantList["apple"] = 1;
+    pq[area].push(Restaurant(2, "aim", 20));
+    restaurantList["aim"] = 2;
+    pq[area].push(Restaurant(3, "air", 30));
+    restaurantList["air"] = 3;
 
-    pq[0].push(Restaurant(1, "apple", 10));
-    pq[0].push(Restaurant(1, "aim", 20));
-    pq[0].push(Restaurant(1, "air", 30));
+    area = 1;
+    mArea.push_back(area);
+    pq[area].push(Restaurant(4, "banana", 10));
+    restaurantList["banana"] = 4;
+    pq[area].push(Restaurant(5, "box", 20));
+    restaurantList["box"] = 5;
+    pq[area].push(Restaurant(6, "boy", 30));
+    restaurantList["boy"] = 6;
 
-    pq[1].push(Restaurant(1, "banana", 10));
-    pq[1].push(Restaurant(1, "box", 20));
-    pq[1].push(Restaurant(1, "boy", 30));
+    area = 2;
+    mArea.push_back(area);
+    pq[area].push(Restaurant(7, "camera", 10));
+    restaurantList["camera"] = 7;
+    pq[area].push(Restaurant(8, "coconut", 20));
+    restaurantList["coconut"] = 8;
+    pq[area].push(Restaurant(9, "covariance", 30));
+    restaurantList["covariance"] = 9;
 
-    for(register int i = 0; i < MAX_CITY; i++){
-        for(register int j = 0; j < pq[i].size(); j++){
+    priority_queue<Restaurant, vector<Restaurant>, cmp> best3;
+
+    for(int i = 0; i < 3; i++){
+        int cnt = 0;
+        while(cnt < 3) {
             Restaurant rest = pq[i].top();
-            cout<<"pq["<<i<<"] = "<<rest.name<<", "<<rest.value<<endl;
+            best3.push(rest);
             pq[i].pop();
+            cnt++;
         }
     }
-
+    
+    int sum = 0;
+    int cnt = 0;
+    while(cnt < 3) {
+        Restaurant rest = best3.top();
+        cout<<"rest = "<<rest.cityID<<", from hash = "<<restaurantList[rest.name]<<", "<<rest.name<<", "<<rest.value<<endl;
+        sum += rest.value;
+        best3.pop();
+        cnt++;
+    }
+    
+    cout<<"sum of best3 = "<<sum<<endl;
     return 0;
 }
